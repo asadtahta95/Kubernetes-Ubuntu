fcd1e6a62df2607bdc1f220d85713f51c03acbe3
As'ad Tahta Alfina <57313918+asadtahta95@users.noreply.github.com>
2022-08-29 15:35:24 +0700

    Create kubernetes-ubuntu.sh

---
+++ kubernetes-ubuntu.sh
@@ -0,0 +1,37 @@
+# Install Kubernetes On Ubuntu by AsadTh95 
+
+# Update packages
+sudo apt update && sudo apt upgrade -y 
+
+#Instalasi Docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+
+#Instalasi Kubernetes
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version
+
+# Install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+mkdir -p /usr/local/bin/
+install minikube /usr/local/bin
+minikube version
+minikube start --driver=docker
+
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
+# Thanks for Contribution
+# AsadTh95
