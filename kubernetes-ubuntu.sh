ef67ba8c49bf3fd66c9927583e7a894f60076383
As'ad Tahta Alfina <57313918+asadtahta95@users.noreply.github.com>
2022-08-29 15:47:52 +0700

    Update kubernetes-ubuntu.sh

--- kubernetes-ubuntu.sh
+++ kubernetes-ubuntu.sh
@@ -23,7 +23,7 @@ kubectl version
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo chmod +x minikube
 sudo mkdir -p /usr/local/bin/
-install minikube /usr/local/bin
+sudo install minikube /usr/local/bin
 minikube version
 minikube start --driver=docker
 
