fc02ad9ff2c82837d500aa1a8e7197b2cea7bea2
As'ad Tahta Alfina <57313918+asadtahta95@users.noreply.github.com>
2022-08-29 15:41:44 +0700

    Update kubernetes-ubuntu.sh

--- kubernetes-ubuntu.sh
+++ kubernetes-ubuntu.sh
@@ -15,21 +15,21 @@ sudo usermod -aG docker ${USER}
 #Instalasi Kubernetes
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version
 
 # Install minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
-mkdir -p /usr/local/bin/
+sudo chmod +x minikube
+sudo mkdir -p /usr/local/bin/
 install minikube /usr/local/bin
 minikube version
 minikube start --driver=docker
 
 # Install Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
+sudo chmod 700 get_helm.sh
 ./get_helm.sh
 
 
